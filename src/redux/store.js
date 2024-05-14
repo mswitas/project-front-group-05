5d7db0a2b0abf3039e101d2cd768a564ae58456b
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-05-14 16:48:11 +0200

    added persisted store sample

---
+++ src/redux/store.js
@@ -0,0 +1,32 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+// import { userReducer } from "./user/slice";
+const authPersistConfig = {
+  key: "auth",
+  storage,
+  whitelist: ["token"],
+};
+
+export const store = configureStore({
+  reducer: {},
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: process.env.NODE_ENV === "development",
+});
+
+export const persistor = persistStore(store);
