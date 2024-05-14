64d77d4804b5ac911b201ec0bbb87d29f8b6f9dd
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-05-14 19:06:03 +0200

    stylizacja poczatek

---
+++ src/components/Balance/Balance.jsx
@@ -0,0 +1,42 @@
+// 6.Znakowanie i style komponentu "Bilans" (telefon, tablet i desktop)
+
+// 7. W polu "Bilans" wyświetlany jest twój bilans w stanie
+
+// 8. Po kliknięciu przycisku "Potwierdzić" kwota bilansu jest wysyłana do backendu i zapisywana w store.
+
+// 11. Naciśnięcie przycisku "Przejść do raportów" powoduje przejście do strony Raportów wydatków
+
+import {
+  BalanceSection,
+  BalanceForm,
+  BalanceText,
+  BalanceInput,
+  BalanceButton,
+  ReportBox,
+  ReportIcon,
+  ReportText,
+} from "./Balance.styled";
+
+const BalanceBar = () => {
+  return (
+    <BalanceSection>
+      <BalanceForm autoComplete="off">
+        <BalanceText id="bilans">Balance:</BalanceText>
+        <BalanceInput
+          id="bilans"
+          name="bilans"
+          type="number"
+          placeholder="00.00 PLN"
+        />
+        <BalanceButton type="submit">CONFIRM</BalanceButton>
+      </BalanceForm>
+      <ReportBox>
+        <ReportText>Reports</ReportText>
+        <ReportIcon>
+          <use href="../../../assets/icons.svg#reports"></use>
+        </ReportIcon>
+      </ReportBox>
+    </BalanceSection>
+  );
+};
+export default BalanceBar;
---
+++ src/components/Balance/Balance.styled.jsx
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+export const BalanceSection = styled.section``;
+export const BalanceForm = styled.form``;
+export const BalanceText = styled.label`
+  font-family: Roboto;
+  font-size: 12px;
+  font-weight: 500;
+  line-height: 14.06px;
+  letter-spacing: 0.02em;
+  text-align: center;
+  color: #52555fb2;
+`;
+export const BalanceInput = styled.input`
+  width: 125px;
+  height: 44px;
+  top: 96px;
+  left: 541px;
+  gap: 0px;
+  border-radius: 16px 0px 0px 0px;
+  border: 2px solid #ffffff;
+  ::placeholder {
+    font-family: Roboto;
+    font-size: 12px;
+    font-weight: 700;
+    line-height: 14.06px;
+    letter-spacing: 0.02em;
+    text-align: right;
+    color: #000000;
+  }
+`;
+
+export const BalanceButton = styled.buttom``;
+
+export const ReportBox = styled.div``;
+export const ReportText = styled.p``;
+export const ReportIcon = styled.svg``;
+
+export const SectionBar = styled.svg`
+  @media (max - width: 767px) {
+    width: ;
+    height: ;
+    padding: ;
+    margin-bottom: 8px;
+  }
+
+  @media (min-width: 768px) {
+  }
+  @media (min-width: 1280px) {
+  }
+`;
