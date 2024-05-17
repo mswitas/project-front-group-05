49e18f9af0de35c781bf5871c0eb46800839895e
Jakub0821 <jakubmigda120@gmail.com>
2024-05-18 01:17:01 +0200

    change hearder,jsx and header.module.css

---
+++ src/components/header/header.jsx
@@ -0,0 +1,10 @@
+import { css } from "../../header/header.module.css";
+import { icons } from "./assets/icons.svg";
+
+export const header =() {
+   return (
+    <div>
+     <
+    </div>
+   )
+};
