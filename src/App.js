c43f8337b702d2866b746b292e6643bcddd68ae4
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-05-18 20:34:13 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
-import React, { lazy, useEffect } from "react";
+import React, { lazy } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { selectIsRefreshing } from "./redux/auth/selectors";
-import { refreshUser } from "./redux/auth/operations";
-import SharedLayout from "./pages/SharedLayout/SharedLayout";
-import { PrivateRoute } from "./pages/PrivateRoute";
-import { RestrictedRoute } from "./pages/RestrictedRoute";
-import Loader from "./service/Loader/Loader";
+// import { useSelector, useDispatch } from "react-redux";
+// import { selectIsRefreshing } from "./redux/auth/selectors";
+// import { refreshUser } from "./redux/auth/operations";
 
+// import { PrivateRoute } from "./pages/PrivateRoute";
+// import { RestrictedRoute } from "./pages/RestrictedRoute";
+// import Loader from "./service/Loader/Loader";
+
+import SharedLayout from "./pages/SharedLayout/SharedLayout";
 const Reports = lazy(() => import("./pages/Reports/Reports"));
 const Expences = lazy(() => import("./pages/Expences/Expences"));
 const Income = lazy(() => import("./pages/Income/Income"));
@@ -18,7 +19,7 @@ const Register = lazy(() => import("./pages/Register/Register"));
 const App = () => {
   return (
     <Routes>
-      {/* <Route path="/" element={<SharedLayout />}> */}
+      <Route path="/" element={<SharedLayout />}>
         <Route index element={<Expences />} />
         <Route path="/expences" element={<Expences />} />
         <Route path="/income" element={<Income />} />
@@ -26,7 +27,7 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="*" element={<Navigate to="/" />} />
-      {/* </Route> */}
+      </Route>
     </Routes>
   );
 };
--- src/pages/SharedLayout/SharedLayout-styled.jsx
+++ src/pages/SharedLayout/SharedLayout-styled.jsx
@@ -1,11 +1,1 @@
-import { Outlet } from "react-router-dom";
 
-const SharedLayout = () => {
-  return (
-    <div>
-      <p>shared layout</p>
-      <Outlet />
-    </div>
-  );
-};
-export default SharedLayout;
