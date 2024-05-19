4aa43eb03f5f958cafae81f1914578876d760c0d
Jakub0821 <jakubmigda120@gmail.com>
2024-05-19 17:09:36 +0200

    change header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -7,11 +7,19 @@ import {
 
 import icons from "../../assets/icons.svg";
 
-export const header =() {
-   return (
-    <div>
+export const render = (
+   <header className="header-border">
+      <div class="container">
+         <nav class="nav-header">
+            <a href="./" rel="noreferrer noopener" class="logo"
+            ><span class="logo-kapusta-font">Kapusta</span>
+            </a>
+            <svg class="logo-header" width="90" height="31">
+            <use href="../../assets/icons.svg"></use>
+          </svg>
+         </nav>         
+      </div>
 
-    </div>
-   )
-};
-export default header;
+   </header>
+)
+export default render;
