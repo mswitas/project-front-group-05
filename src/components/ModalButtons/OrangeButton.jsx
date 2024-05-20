a330f585f3c4d1db9fc21ad14f2edd850a81659d
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-05-20 17:24:10 +0200

    Added header, aded shared layout

---
+++ src/components/ModalButtons/OrangeButton.jsx
@@ -0,0 +1,32 @@
+import PropTypes from "prop-types";
+import { StyledOrangeButton } from "./Buttons.styled";
+
+// Shared button for modal windows
+export const OrangeButton = ({
+  children,
+  dispatch,
+  closeModal,
+  changeBalance,
+}) => {
+  const handleClick = () => {
+    if (children === "YES") {
+      dispatch();
+      closeModal();
+    }
+    if (changeBalance) {
+      dispatch();
+      closeModal();
+    }
+  };
+
+  return (
+    <StyledOrangeButton onClick={handleClick}>{children}</StyledOrangeButton>
+  );
+};
+
+OrangeButton.propTypes = {
+  children: PropTypes.string.isRequired,
+  dispatch: PropTypes.func,
+  closeModal: PropTypes.func,
+  changeBalance: PropTypes.func,
+};
--- src/pages/SharedLayout/SharedLayout-styled.jsx
+++ src/pages/SharedLayout/SharedLayout-styled.jsx
@@ -1,1 +1,15 @@
 
+import styled from "styled-components";
+
+export const SharedLayoutContainer = styled.div`
+  width: 320px;
+  margin: 0px auto;
+
+  @media screen and (min-width: 768px) {
+    width: 768px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    width: 1280px;
+  }
+`;
