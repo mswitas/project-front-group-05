ad8c15199e05cdc4cb0a00446501bf8122f95ea4
mswitas <mswitas@gmail.com>
2024-05-21 15:02:47 +0200

    add chart component

--- src/pages/Reports/Reports.jsx
+++ src/pages/Reports/Reports.jsx
@@ -1,9 +1,13 @@
 import BalanceReports from "../../components/Balance/BalanceReports/BalanceReports";
+import Chart from "../../components/Chart/Chart";
 
 const Reports = () => {
+  const data = [35, 51, 15, 60, 70, 12];
+  const labels = ['L1', 'L2', 'L3', 'L4', 'L5', 'L6'];
   return (
     <div>
       <BalanceReports />
+      <Chart data={data} labels={labels} />
     </div>
   );
 };
