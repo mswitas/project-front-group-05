06bfdc560f8bc85ed12a45d1069e74d4616debd9
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-06-02 14:26:39 +0200

    commented even more

--- src/components/TransactionsList/TransactionListDesktop.jsx
+++ src/components/TransactionsList/TransactionListDesktop.jsx
@@ -1,61 +1,61 @@
-import { useDispatch, useSelector } from "react-redux";
+// import { useDispatch, useSelector } from "react-redux";
 import PropTypes from "prop-types";
 
-import { selectIsLoading } from "../../redux/transactions/selectors";
-import { deleteTransaction } from "../../redux/transactions/operations";
+// import { selectIsLoading } from "../../redux/transactions/selectors";
+// import { deleteTransaction } from "../../redux/transactions/operations";
 
 import { TransactionTable } from "./TransactionListDesktop.styled";
 // import icons from "../../assets/icons.svg";
 
 export const TransactionListDesktop = ({ children }) => {
   //Translation
-  const categoryTranslateEng = (name) => {
-    switch (name.trim()) {
-      case "Продукты":
-        return "Products";
-      case "Алкоголь":
-        return "ЗСУ";
-      case "Развлечения":
-        return "Entertainment";
-      case "Здоровье":
-        return "Health";
-      case "Транспорт":
-        return "Transport";
-      case "Всё для дома":
-        return "Housing";
-      case "Техника":
-        return "Technics";
-      case "Коммуналка и связь":
-        return "Communal and communication";
-      case "Спорт и хобби":
-        return "Sport and hobby";
-      case "Образование":
-        return "Education";
-      case "Прочее":
-        return "Other";
-      case "З/П":
-        return "Salary";
-      case "Доп. доход":
-        return "Additional income";
-      default:
-        break;
-    }
-  };
+  // const categoryTranslateEng = (name) => {
+  //   switch (name.trim()) {
+  //     case "Продукты":
+  //       return "Products";
+  //     case "Алкоголь":
+  //       return "ЗСУ";
+  //     case "Развлечения":
+  //       return "Entertainment";
+  //     case "Здоровье":
+  //       return "Health";
+  //     case "Транспорт":
+  //       return "Transport";
+  //     case "Всё для дома":
+  //       return "Housing";
+  //     case "Техника":
+  //       return "Technics";
+  //     case "Коммуналка и связь":
+  //       return "Communal and communication";
+  //     case "Спорт и хобби":
+  //       return "Sport and hobby";
+  //     case "Образование":
+  //       return "Education";
+  //     case "Прочее":
+  //       return "Other";
+  //     case "З/П":
+  //       return "Salary";
+  //     case "Доп. доход":
+  //       return "Additional income";
+  //     default:
+  //       break;
+  //   }
+  // };
   // Dispatch
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   // Selectors
-  const isLoading = useSelector(selectIsLoading);
+  // const isLoading = useSelector(selectIsLoading);
   // Green if income and red if expenses
-  const color = children[1];
-  let minus = "-";
+  // const color = children[1];
+  // let minus = "-";
 
-  if (color === "green") {
-    minus = false;
-  }
+  // if (color === "green") {
+  //   minus = false;
+  // }
   // Handle delete transaction
-  const handleDelete = (event) => {
-    dispatch(deleteTransaction(event.currentTarget.id));
-  };
+  // const handleDelete = (event) => {
+  //   dispatch(deleteTransaction(event.currentTarget.id));
+  // };
   // Sorted transactions
 //   const sortedTransactions = children[0].slice().sort((a, b) => {
 //     const first = new Date(a.date).getTime();
