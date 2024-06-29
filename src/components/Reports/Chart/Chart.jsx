c22c4b294f0360f4e35752759505444995f814ff
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-06-29 22:13:38 +0200

    mobilee char visual in progress

--- src/components/Reports/Chart/Chart.jsx
+++ src/components/Reports/Chart/Chart.jsx
@@ -1,16 +1,15 @@
 // import { useMatchMedia } from "../../../hooks/MediaQuery";
 import { ChartWrapper, SectionWrapper } from "./Chart.styled";
-import { BarChart, Bar, XAxis, LabelList } from "recharts";
-const colors = ["#FF751D", "#FFDAC0"];
+import { Tooltip, BarChart, Bar, XAxis, YAxis, LabelList } from "recharts";
+
 const data = [
   { name: "Chicken", value: 1000, fill: "#FF751D" },
   { name: "Fuel", value: 500, fill: "#FFDAC0" },
   { name: "Beef", value: 8000, fill: "#FFDAC0" },
   // ...
 ];
-// "#FF751D";
-const Chart = () => {
-  const activeIds = [0, 3, 6, 9, 12, 15];
+
+const Char = () => {
   return (
     <SectionWrapper>
       <ChartWrapper>
@@ -18,7 +17,7 @@ const Chart = () => {
           width={758}
           height={422}
           data={data}
-          margin={{ top: 30, right: 0, bottom: 0, left: 0 }}
+          margin={{ top: 30, right: 76, bottom: 0, left: 77 }}
           style={{
             color: "#52555F",
             fontFamily: "Roboto",
@@ -65,4 +64,120 @@ const Chart = () => {
   );
 };
 
+const TabletChart = () => {
+  return (
+    <SectionWrapper>
+      <ChartWrapper>
+        <BarChart
+          width={635}
+          height={422}
+          data={data}
+          margin={{ top: 30, right: 15, bottom: 0, left: 15 }}
+          style={{
+            color: "#52555F",
+            fontFamily: "Roboto",
+            fontSize: 12,
+            fontWeight: 400,
+            lineHeight: 14.06,
+            letterSpacing: 0.02,
+            textAlign: "center",
+          }}
+        >
+          <defs>
+            <pattern
+              id="stripes"
+              width="100%"
+              height="40"
+              patternUnits="userSpaceOnUse"
+              y="-10"
+            >
+              <rect x="0" y="0" width="100%" height="2" fill="#F5F6FB" />
+              <rect x="0" y="2" width="100%" height="40" fill="#FFFFFF" />
+            </pattern>
+          </defs>
+          <rect x={0} y={0} width={635} height={422} fill="url(#stripes)" />
+
+          <XAxis dataKey="name" axisLine={false} tickLine={false}></XAxis>
+          <Bar
+            dataKey="value"
+            barSize={38}
+            radius={[10, 10, 0, 0]}
+            itemStyle={(props) => ({ fill: props.payload.fill })}
+          >
+            <LabelList
+              dataKey="value"
+              position="top"
+              formatter={(value) => `${value} UAH`}
+              style={{ whiteSpace: "nowrap" }}
+              width={100}
+              fill="#52555F"
+            />
+          </Bar>
+        </BarChart>
+      </ChartWrapper>
+    </SectionWrapper>
+  );
+};
+
+// const Chart = () => {
+//   return (
+//     <SectionWrapper>
+//       <ChartWrapper>
+//         <BarChart
+//           width={280}
+//           height={495}
+//           data={data}
+//           margin={{ top: 30, right:10, bottom: 0, left: 10 }}
+//           style={{
+//             color: "#52555F",
+//             fontFamily: "Roboto",
+//             fontSize: 12,
+//             fontWeight: 400,
+//             lineHeight: 14.06,
+//             letterSpacing: 0.02,
+//             textAlign: "center",
+//           }}
+//         >
+//           <YAxis dataKey="name" axisLine={true} tickLine={false}></YAxis>
+//           <Bar
+//             dataKey="value"
+//             barSize={15}
+//             radius={[10, 10, 0, 0]}
+//             itemStyle={(props) => ({ fill: props.payload.fill })}
+//           >
+//             <LabelList
+//               dataKey="value"
+//               position="right"
+//               formatter={(value) => `${value} UAH`}
+//               style={{ whiteSpace: "nowrap" }}
+//               width={100}
+//               fill="#52555F"
+//             />
+//           </Bar>
+//         </BarChart>
+//       </ChartWrapper>
+//     </SectionWrapper>
+//   );
+// };
+const Chart = () => {
+  return (
+    <BarChart width={280} height={495} data={data}>
+      <defs>
+        <linearGradient id="bg" x1="0" y1="0" x2="0" y2="1">
+          <stop offset="5%" stopColor="#ffffff" stopOpacity={1} />
+          <stop offset="95%" stopColor="#ffffff" stopOpacity={1} />
+        </linearGradient>
+      </defs>
+      <rect x={0} y={0} width={500} height={600} fill="url(#bg)" />
+      <YAxis type="category" dataKey="name" />
+      <Bar
+        dataKey="value"
+        fill="red"
+        barSize={15}
+        radius={[10, 10, 0, 0]}
+      />
+      <Tooltip />
+    </BarChart>
+  );
+};
 export default Chart;
